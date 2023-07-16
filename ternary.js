9e57fd427ae54666966ed5d1c945ebff8f218604
kiatklai <likhitthaworn.k@gmail.com>
2023-07-16 21:01:18 +0900

    ternary operator

---
+++ ternary.js
@@ -0,0 +1,16 @@
+//Ternary Operator 方法で簡潔に書く
+
+let score =51
+
+//original type
+//let result
+// if(score >= 50){
+//   result="合格"
+// }else{
+//   result="不合格"
+// }
+// console.log(result)
+
+//Ternary Operator ? true : false 
+let result = score>=50 ? "合格" : "不合格"
+console.log(result)
